f69ad9b84df6ff1d6ed98e42613ef3fa429683e0
Elias Lecomte <elias.lecomte@gmail.com>
2022-04-12 23:42:13 +0200

    fix: use the new content options for tailwindcss

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,16 +3,13 @@ module.exports = {
     removeDeprecatedGapUtilities: true,
     purgeLayersByDefault: true,
   },
-  purge: {
-    enabled: Boolean(process.env.NODE_ENV === 'production'),
-    content: [
-      './core/**/*.tsx',
-      './pages/**/*.tsx',
-      './meetnet/components/**/*.tsx',
-      './openWeather/components/**/*.tsx',
-      './windfinder/components/**/*.tsx',
-    ],
-  },
+  content: [
+    './core/**/*.tsx',
+    './pages/**/*.tsx',
+    './meetnet/components/**/*.tsx',
+    './openWeather/components/**/*.tsx',
+    './windfinder/components/**/*.tsx',
+  ],
   theme: {
     extend: {},
   },
